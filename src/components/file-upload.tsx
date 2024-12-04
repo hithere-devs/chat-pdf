ac92c95ed80da1b452fc7165cea9f448f55d5774
Azhar Mahmood <mywritingfrenzy@gmail.com>
2024-12-04 22:19:35 +0530

    Added Toast

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -1,11 +1,30 @@
 'use client';
 
 import { uploadToS3 } from '@/lib/s3';
+import { useMutation } from '@tanstack/react-query';
 import { Inbox } from 'lucide-react';
 import React from 'react';
 import { useDropzone } from 'react-dropzone';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const FileUpload = () => {
+	const { mutate } = useMutation({
+		mutationFn: async ({
+			file_key,
+			file_name,
+		}: {
+			file_key: string;
+			file_name: string;
+		}) => {
+			const response = await axios.post('/api/create-chat', {
+				file_key,
+				file_name,
+			});
+			return response.data;
+		},
+	});
+
 	const { getRootProps, getInputProps } = useDropzone({
 		accept: { 'application/pdf': ['.pdf'] },
 		maxFiles: 1,
@@ -16,12 +35,24 @@ const FileUpload = () => {
 			const file = acceptedFiles[0];
 			if (file.size > 1024 * 1024 * 10) {
 				// bigger than 10MB
-				alert('File size is bigger than 10MB');
+				toast.error('File too large, bigger than 10MB');
+				// alert('File size is bigger than 10MB');
 				return;
 			}
 			try {
 				const data = await uploadToS3(file);
-				console.log('data: ', data);
+				if (!data?.file_key || !data?.file_name) {
+					toast.error('Failed to upload file');
+					return;
+				}
+				mutate(data, {
+					onSuccess: (data) => {
+						console.log(data);
+					},
+					onError: (error) => {
+						toast.error('Error creating chat');
+					},
+				});
 			} catch (error) {
 				console.log(error);
 			}
